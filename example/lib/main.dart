4a78290a09e1d3bb38b3b1397abaa4844c698ea8
buyka <buyankhishig@tapatrip.com>
2023-03-28 18:11:50 +0800

    passport scan

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+import 'package:example/screens/camera_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mrz_scanner/mrz_scanner.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyApp());
 }
 
@@ -11,45 +16,65 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final MRZController controller = MRZController();
-
+  File? image;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Builder(builder: (context) {
-        return MRZScanner(
-          controller: controller,
-          onSuccess: (mrzResult) async {
-            await showDialog(
-              context: context,
-              builder: (context) => Dialog(
-                insetPadding: const EdgeInsets.symmetric(horizontal: 10),
-                child: SingleChildScrollView(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                          controller.currentState?.resetScanning();
-                        },
-                        child: const Text('Reset Scanning'),
-                      ),
-                      Text('Name : ${mrzResult.givenNames}'),
-                      Text('Gender : ${mrzResult.sex.name}'),
-                      Text('CountryCode : ${mrzResult.countryCode}'),
-                      Text('Date of Birth : ${mrzResult.birthDate}'),
-                      Text('Expiry Date : ${mrzResult.expiryDate}'),
-                      Text('DocNum : ${mrzResult.documentNumber}'),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        );
-      }),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Builder(builder: (context) {
+          return Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (image != null) Image.file(image!),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+                        return CameraScreen();
+                      }));
+                    },
+                    child: Text("scan with camera")),
+                ElevatedButton(
+                    onPressed: () async {
+                      var mrzResult = await MrzWithGallery().pickImage();
+
+                      if (mrzResult != null) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => Dialog(
+                            insetPadding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: SingleChildScrollView(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Reset Scanning'),
+                                  ),
+                                  Text('Name : ${mrzResult.givenNames}'),
+                                  Text('Gender : ${mrzResult.sex.name}'),
+                                  Text('CountryCode : ${mrzResult.countryCode}'),
+                                  Text('Date of Birth : ${mrzResult.birthDate}'),
+                                  Text('Expiry Date : ${mrzResult.expiryDate}'),
+                                  Text('DocNum : ${mrzResult.documentNumber}'),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    child: Text("scan with gallery")),
+              ],
+            ),
+          );
+        }),
+      ),
     );
   }
 }
--- lib/src/mrz_helper.dart
+++ lib/src/mrz_helper.dart
@@ -1,22 +1,38 @@
 class MRZHelper {
   static List<String>? getFinalListToParse(List<String> ableToScanTextList) {
+    List<String> resultList = [];
     if (ableToScanTextList.length < 2) {
       // minimum length of any MRZ format is 2 lines
       return null;
     }
-    int lineLength = ableToScanTextList.first.length;
-    for (var e in ableToScanTextList) {
-      if (e.length != lineLength) {
-        return null;
+    int? lineLength;
+    if (ableToScanTextList.first.substring(0, 5) == "PEMNG") {
+      lineLength = 44;
+    } else {
+      lineLength = ableToScanTextList.first.length;
+      if (lineLength > 36) {
+        lineLength = 44;
+      } else if (lineLength > 30) {
+        lineLength = 36;
+      } else {
+        lineLength = 30;
       }
+    }
+
+    for (var e in ableToScanTextList) {
+      if (e.length > lineLength) {
+        e = e.substring(0, lineLength);
+      } else if (e.length < lineLength) {
+        e = e + ("<" * (lineLength - e.length));
+      }
+      resultList.add(e);
       // to make sure that all lines are the same in length
     }
-    List<String> firstLineChars = ableToScanTextList.first.split('');
+    List<String> firstLineChars = resultList.first.split('');
     List<String> supportedDocTypes = ['A', 'C', 'P', 'V', 'I'];
     String fChar = firstLineChars[0];
-    String sChar = firstLineChars[1];
-    if (sChar == '<' && supportedDocTypes.contains(fChar)) {
-      return [...ableToScanTextList];
+    if (supportedDocTypes.contains(fChar)) {
+      return resultList;
     }
     return null;
   }
@@ -26,7 +42,7 @@ class MRZHelper {
     List<String> list = res.split('');
 
     // to check if the text belongs to any MRZ format or not
-    if (list.length != 44 && list.length != 30 && list.length != 36) {
+    if (list.length < 30 || !list.contains('<')) {
       return '';
     }
 
@@ -35,8 +51,7 @@ class MRZHelper {
         list[i] = list[i].toUpperCase();
         // to ensure that every letter is uppercase
       }
-      if (double.tryParse(list[i]) == null &&
-          !(RegExp(r'^[A-Za-z0-9_.]+$').hasMatch(list[i]))) {
+      if (double.tryParse(list[i]) == null && !(RegExp(r'^[A-Za-z0-9_.]+$').hasMatch(list[i]))) {
         list[i] = '<';
         // sometimes < sign not recognized well
       }
